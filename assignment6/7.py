ca368246f8bbbac63143b20a9a2c6ef4a0ad0e79
Raja251 <rajasimha.2510@gamil.com>
2025-05-22 17:34:01 +0530

    3

--- assignment6/7.py
+++ assignment6/7.py
@@ -7,7 +7,7 @@ students={
 userinput=input("enter student name: ")
 if userinput in students:
     avg=(students[userinput][0]+students[userinput][1]+students[userinput][2])/3
-    print(f"average ={avg}")
+    print(f"average ={avg}")   
     if avg >= 60:
         print("pass")
     else:
